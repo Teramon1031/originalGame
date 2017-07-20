55a85f98a3eceaff2bebcec955f695f7b2636af6
寺門美緒 <teramon.jump02@gmail.com>
2017-07-20 23:13:29 +0900

    ステージ大体完成

---
+++ Assets/Scripts/ChalkInstantiateScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChalkInstantiateScript : MonoBehaviour {
+
+	public GameObject chalk; 
+
+	void Start () {
+		for (int i = 0; i < 10; i++) {
+			Instantiate (chalk, new Vector3 
+				(Random.Range (-25.0f, 25.0f), 3.05f, Random.Range (-25.0f, 25.0f)), transform.rotation);
+		}
+	}
+
+	void Update () {
+		
+	}
+}
