a467234784509043a7360a582e70c118fc11ff74
寺門美緒 <teramon.jump02@gmail.com>
2017-08-07 00:11:29 +0900

    敵に歩くアニメーションを実装 unityちゃんに投げるアニメーションを実装途中 MainCameraの位置がおかしい

---
+++ Assets/Scripts/EnemyInstantiatescript.cs
@@ -0,0 +1,60 @@
+﻿
+//プレイヤーが近づいた時&生成ポイントが視界外の時敵を生成する
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInstantiatescript : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject enemy;
+	private bool PlayerIsNear;
+	private bool IsRenderered;
+	private bool TimeInterval;
+	public float timer;
+
+	void Start () {
+		timer = 0.0f;
+	}
+
+	void Update () {
+	//プレイヤーと生成ポイントの距離を取得し10未満ならtrue,以上ならfalse
+		Vector3 PosA = player.transform.position;
+		Vector3 PosB = this.transform.position;
+		float dis = Vector3.Distance (PosA,PosB);
+
+		if (dis < 10) {
+			PlayerIsNear = true;
+		} else { 
+			PlayerIsNear = false;
+		}
+	
+	//プレイヤーが生成ポイントに近づいたかつカメラに写っていない時に生成
+		timer -= Time.deltaTime;
+		if (timer <= 0.0f) {
+			if (IsRenderered == false && PlayerIsNear) { 
+				Instantiate (enemy, this.transform.position, transform.rotation);
+			}
+			timer = 3.0f;
+			IsRenderered = false;
+		}
+
+	}
+
+	private void OnWillRenderObject(){
+		//メインカメラに映った時だけIsRenderedをfalseに
+
+		#if UNITY_EDITOR
+
+		if (Camera.current.name != "SceneCamera" && Camera.current.name != "Preview Camera")
+		
+		#endif
+		
+		{
+			if (Camera.current.tag == "MainCamera") {
+				IsRenderered = true;
+			}
+		}
+	}
+}
--- Assets/Scripts/muzzleScript.cs
+++ Assets/Scripts/muzzleScript.cs
@@ -9,7 +9,13 @@ public class muzzleScript : MonoBehaviour {
 	public GameObject ChalkGrenade;
 	private int ChalkNumber;
 	public static int ChalkPowder = 10;
+	public GameObject player;
+	public Animator animator;
 //	public Text ChalkNumberText;
+
+	void Awake () {
+		animator = player.GetComponent<Animator> ();
+	}
 
 	void Start () {
 //		ChalkNumber = 10;
@@ -19,16 +25,22 @@ public class muzzleScript : MonoBehaviour {
 
 	void Update () {
 //		if (ChalkNumber > 0) {
-			if (Input.GetKeyDown (KeyCode.Alpha1)) {
-				Instantiate (Chalk, this.transform.position, this.transform.rotation);
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			Instantiate (Chalk, this.transform.position, this.transform.rotation);
+			animator.SetBool ("Throw", true);
 //				ChalkNumber--;
-			}
+		} else {
+			animator.SetBool ("Throw", false);
+		}
 //		}
 //		if (ChalkPowder >= 5) {
-			if (Input.GetKeyDown (KeyCode.Alpha2)) {
-				Instantiate (ChalkGrenade, this.transform.position, this.transform.rotation);
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			Instantiate (ChalkGrenade, this.transform.position, this.transform.rotation);
+			animator.SetBool ("Throw", true);
 //				ChalkPowder -= 5;
-			}
+		} else {
+			animator.SetBool ("Throw", false);
+		}
 //		}
 //		ChalkNumberText.text = "Chalk : \t" + ChalkNumber.ToString ();
 	}
